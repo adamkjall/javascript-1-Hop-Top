8075aac141cf5cb7957a9e37526caeafb1ec93e4
Claudia Aranda <claudia_15_@hotmail.com>
2019-12-16 12:03:38 +0100

    blocks

--- sketch/block.ts
+++ sketch/block.ts
@@ -1,14 +1,18 @@
-// class Block extends GameObject {
-// constructor(position: Position, width: number, height: number){
-//     super(position, width, height,"")
-// }
+class Block extends GameObject {
+constructor(position: Position, width: number, height: number){
+    super(position, width, height,"")
+}
 
 
 
 
-//     // public getPosition(): Position {}
-//     public getWith(): number {}
-//     public getHeight() : number ()
+    // public getPosition(): Position {}
+    public getWith(): number {
+        return 0
+    }
+    public getHeight() : number {
+        return 0
+    }
 
-// }
+}
 
