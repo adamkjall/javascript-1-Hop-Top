797712ed2d5174f6758c542ecdc555dc93567482
Adam <adam.kjall@hotmail.com>
2019-12-19 14:44:31 +0100

    refactored sketch and gameController

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -22,12 +22,8 @@ let imgSolid: p5.Image;
 let imgFragile: p5.Image;
 let imgItemStar: p5.Image;
 let collectItemSound: p5.SoundFile;
-let player: Player;
-let level : Level;
-let levelFactory: LevelFactory;
+
 let gameController: GameController;
-let collisionDetection: CollisionDetection;
-let score : number = 0;
 
 /**
  * Built in setup function in P5
@@ -38,15 +34,8 @@ let score : number = 0;
 function setup() {
   createCanvas(600, windowHeight);
   frameRate(60);
-  player = new Player(width/2, height/2)
-  levelFactory = new LevelFactory();
-  level = levelFactory.createLevel(1);
 
-  gameController = new GameController(level, player,1, 0, 0 ,0);
-
-  collisionDetection = new CollisionDetection();
-
-
+  gameController = new GameController();
 }
 /**
  * Built in draw function in P5
@@ -55,29 +44,7 @@ function setup() {
  */
 
 function draw() {
-  background("cornflowerblue");
-  level.drawLevel();
-  player.move();
-  player.drawPlayer();
-
-  gameController.displayScoreBoard();
-
-  // TODO: rename block to game object name,
-  //   because bonus items could be also in collision with player
-  level.levelObjects.forEach(block => {
-    if(collisionDetection.playerCollidedWithBlock(player, block)) {
-      if (block instanceof Item) {
-        const item = block as Item;
-        item.explode()
-        gameController.collectItem()
-      } else {
-        player.bounceOnBlock(block.pos);
-      }
-    }
-  })
-
-  level.updateLevel(player.pos);
-
+  gameController.gameLoop();
 }
 /**
  *  Built in windowResize listener function in P5
