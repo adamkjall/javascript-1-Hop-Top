347db55edfaba7e4352af904a5ea03278dfedc70
petelina17 <petelina17@gmail.com>
2019-12-20 13:22:56 +0100

    fix collecting multiple scores

--- sketch/gameController.ts
+++ sketch/gameController.ts
@@ -26,7 +26,7 @@ class GameController {
     this.level.updateLevel(this.player.pos);
 
     // moves all level objects down
-    this.level.levelObjects.forEach(levelObject => {
+    this.level.levelObjects.forEach((levelObject, index, object) => {
       if (
         this.collisionDetection.playerCollidedWithBlock(
           this.player,
@@ -35,10 +35,12 @@ class GameController {
       ) {
         if (levelObject instanceof Item) {
           const item = levelObject as Item;
+          object.splice(index, 1);
           item.explode();
           gameController.collectItem();
         } else if (levelObject instanceof SpeedBoost) {
           const item = levelObject as SpeedBoost;
+          object.splice(index, 1);
           item.explode();
           gameController.collectItem();
           this.player.speedBoost();
--- sketch/item.ts
+++ sketch/item.ts
@@ -1,25 +1,22 @@
 class Item extends GameObject {
     private _img: p5.Image;
-    private _score: number;
+    private _exploded: boolean = false;
 
     constructor(
         x: number,
         y: number,
         width: number,
         height: number,
-        image: p5.Image,
-        score: number,
+        image: p5.Image
     ) {
         super(x, y, width, height);
         this._img = image;
-        this._score = score;
-    }
-
-    public getScore(): number {
-        return this._score;
     }
 
     public drawObject(): void {
+        if (this._exploded) {
+            return;
+        }
         push();
         image(
             this._img,
@@ -30,4 +27,16 @@ class Item extends GameObject {
         );
         pop();
     }
+
+    public explode(): void {
+        if (this._exploded) {
+            return;
+        }
+        this._exploded = true;
+        // alert('boom!!!');
+    }
+
+
+
+
 }
