dc9c5654a7df0986a9526d282d439671a076aa63
petelina17 <petelina17@gmail.com>
2019-12-29 20:46:40 +0100

    make various item scores to be collected to score board

--- sketch/gameController.ts
+++ sketch/gameController.ts
@@ -66,7 +66,8 @@ class GameController {
       ) {
         if (levelObject instanceof Item) {
           this.level.levelObjects.splice(index, 1);
-          this.collectItem();
+          const itemScore = levelObject.getScore();
+          this.collectItem(itemScore);
           const effect = new Effect(levelObject);
           this.effectList.push(effect);
         } else if (levelObject instanceof SpeedBoost) {
@@ -154,8 +155,8 @@ class GameController {
     pop();
   }
 
-  private collectItem(): void {
-    this.score += 20;
+  private collectItem(itemScore: number): void {
+    this.score += itemScore; //20;
     if (this.score >= this.highScore) {
       this.highScore = this.score;
     }
