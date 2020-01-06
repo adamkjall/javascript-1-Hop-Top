57b010924f5c7b028b1db02589fd2e0f62459b1e
Adam <adam.kjall@hotmail.com>
2020-01-06 19:23:59 +0100

    implemented input field for name in startscreen and local storage now keeps track of different players highscore

--- sketch/gameController.ts
+++ sketch/gameController.ts
@@ -9,18 +9,22 @@ class GameController {
   private isStartingNextLevel: boolean;
   private countDown: number;
   private isStartGame: boolean;
+  private isGameOver: boolean;
   private playButton: p5.Element | undefined;
   private quitButton: p5.Element | undefined;
-  private startScreen: StartScreen;
+  private startScreen: StartScreen | undefined;
   private scoreboard: Scoreboard;
 
-
-  constructor() {
+  constructor(showStartScreen: boolean = true) {
     this.score = 0;
     const localStorageHighscore = localStorage.getItem("highscore");
-    this.highScore = localStorageHighscore
+    const highscoreObj = localStorageHighscore
       ? JSON.parse(localStorageHighscore)
-      : 0;
+      : {};
+    const highscoreArray = Object.keys(highscoreObj).map(
+      key => highscoreObj[key]
+    );
+    this.highScore = highscoreArray.length ? Math.max(...highscoreArray) : 0;
     this.levelNumber = 1;
     this.levelFactory = new LevelFactory();
     this.level = this.levelFactory.createLevel(this.levelNumber);
@@ -28,11 +32,11 @@ class GameController {
     this.collisionDetection = new CollisionDetection();
     this.isStartingNextLevel = false;
     this.countDown = 5;
-    this.isStartGame = true;
-    this.startScreen = new StartScreen();
+    this.isStartGame = showStartScreen;
+    this.isGameOver = false;
+    this.startScreen = showStartScreen ? new StartScreen() : undefined;
     this.scoreboard = new Scoreboard();
   }
-  
 
   public drawStartScreen() {
     push();
@@ -49,25 +53,30 @@ class GameController {
   }
 
   public drawGame(): void {
-    if ((keyIsPressed && keyCode === 32) || mouseIsPressed === true) {
+    if (this.isStartGame && keyIsPressed && keyCode === 13) {
+      removeElements();
       this.isStartGame = false;
     }
-    if (this.isStartGame) {
+    if (this.isStartGame && this.startScreen) {
       this.startScreen.draw();
       return;
     }
 
     //If player is under game area display Game Over on screen
     if (this.isPlayerDead()) {
+      if (!this.isGameOver) {
+        console.log("saved");
+
+        this.saveHighscore();
+      }
+      this.isGameOver = true;
       this.displayGameOver();
-      localStorage.setItem("highscore", JSON.stringify(this.highScore));
       return;
     }
 
     // if level is done and we're not starting a new level
     if (this.level.levelProgress >= 100 && !this.isStartingNextLevel) {
       this.startNextLevel();
-      localStorage.setItem("highscore", JSON.stringify(this.highScore));
     }
 
     this.player.move();
@@ -120,6 +129,24 @@ class GameController {
     if (this.isStartingNextLevel) this.displayCountDown();
   }
 
+  private saveHighscore() {
+    const localStorageName = localStorage.getItem("name");
+    const playerName = localStorageName ? JSON.parse(localStorageName) : "";
+
+    if (playerName) {
+      const localStorageHighscore = localStorage.getItem("highscore");
+      const highscore = localStorageHighscore
+        ? JSON.parse(localStorageHighscore)
+        : {};
+      if (highscore[playerName] && highscore[playerName] > this.score) {
+        highscore[playerName] = this.score;
+      } else {
+        highscore[playerName] = this.score;
+      }
+      localStorage.setItem("highscore", JSON.stringify(highscore));
+    }
+  }
+
   private isPlayerDead = (): boolean =>
     this.player.pos.y > height + this.player.radius * 2;
 
@@ -155,7 +182,7 @@ class GameController {
     text("Next level in " + this.countDown, width / 2, height / 4);
     pop();
   }
- displayGameOver() {
+  displayGameOver() {
     if (!this.playButton && !this.quitButton) {
       push();
       //if clicked go to level_1
@@ -164,7 +191,7 @@ class GameController {
       this.playButton.position(windowWidth / 2, height * 0.82);
       this.playButton.center("horizontal");
       this.playButton.style("background-color", "rgb(252, 208, 107)");
-      this.playButton.style('font-family', 'Amatic SC');
+      this.playButton.style("font-family", "Amatic SC");
       //textFont(font);
       this.playButton.style("font-size", "2rem");
       this.playButton.style("color", "rgb(38,48,86)");
@@ -180,7 +207,7 @@ class GameController {
       this.quitButton.position(windowWidth / 2, height * 0.94);
       this.quitButton.center("horizontal");
       this.quitButton.style("background-color", "rgb(38,48,86)");
-      this.quitButton.style('font-family', 'Amatic SC');
+      this.quitButton.style("font-family", "Amatic SC");
       //textFont(font);
       this.quitButton.style("font-size", "1.7rem");
       this.quitButton.style("color", "rgb(252, 208, 107)");
@@ -205,15 +232,14 @@ class GameController {
     pop();
   }
 
-
-
   private restartGame(): void {
     removeElements();
-    gameController = new GameController();
+    gameController = new GameController(false);
   }
 
   private quitGame(): void {
-    location.reload();
+    removeElements();
+    gameController = new GameController();
   }
 
   private updateScore(itemScore: number): void {
@@ -223,5 +249,4 @@ class GameController {
       this.highScore = this.score;
     }
   }
-
 }
