b4d1d91d4f9d6ee22806d4df11cf8e7197fd8263
Adam <adam.kjall@hotmail.com>
2020-01-06 19:48:59 +0100

    removed unecessary code

--- sketch/gameController.ts
+++ sketch/gameController.ts
@@ -32,20 +32,6 @@ class GameController {
     this.scoreboard = new Scoreboard();
   }
 
-  public drawStartScreen() {
-    push();
-    textFont(font);
-    background("#acb8e5");
-    imageMode(CENTER);
-    image(hopTopImage, width / 2, height * 0.45, width * 0.75);
-    fill("white");
-    textAlign(CENTER);
-    textSize(30);
-    text("click on screen to", width / 2, height * 0.91);
-    text("start the game", width / 2, height * 0.95);
-    pop();
-  }
-
   public drawGame(): void {
     if ((keyIsPressed && keyCode === 32) || mouseIsPressed === true) {
       this.isStartGame = false;
