de1b9bbdc34703fbabdf7ea4554452928bda8953
Claudia Aranda <claudia_15_@hotmail.com>
2020-01-07 16:40:37 +0100

    made gameover image smaller and fixed buttons

--- sketch/GameOverSreen.ts
+++ sketch/GameOverSreen.ts
@@ -7,7 +7,7 @@ class GameOver {
       push();
       //if clicked go to level1
       this.playButton = createButton("PLAY AGAIN?");
-      this.playButton.position(windowWidth / 2, height * 0.82);
+      this.playButton.position(windowWidth/ 2, height * 0.82);
       this.playButton.center("horizontal");
       this.playButton.style("background-color", "rgb(252, 208, 107)");
       this.playButton.style('font-family', 'AmaticSC-Bold')
@@ -17,34 +17,37 @@ class GameOver {
       this.playButton.style("padding", "1rem");
       this.playButton.style("border", "none");
       this.playButton.style("outline", "none");
-
+      this.playButton.style("transform", "translateX(-50%)")
+      this.playButton.style("left", "35%")
       this.playButton.mousePressed(this.restartGame);
 
       //if clicked go to startScreen?
-      this.quitButton = createButton("QUIT");
-      this.quitButton.position(windowWidth / 2, height * 0.94);
+      this.quitButton = createButton("QUIT GAME");
+      this.quitButton.position(windowWidth/ 2, height * 0.82);
       this.quitButton.center("horizontal");
       this.quitButton.style("background-color", "rgb(38,48,86)");
       this.quitButton.style('font-family', 'AmaticSC-Bold')
-      this.quitButton.style("font-size", "1.3rem");
+      this.quitButton.style("font-size", "1.7rem");
       this.quitButton.style("color", "rgb(252, 208, 107)");
-      this.quitButton.style("border-radius", "1rem");
+      this.quitButton.style("border-radius", "2rem");
+      this.quitButton.style("padding", "1rem");
       this.quitButton.style("border", "none");
       this.quitButton.style("outline", "none");
-      this.quitButton.style("display", "grid");
-      this.quitButton.style("justify-items", "center");
+      this.quitButton.style("transform", "translateX(-50%)")
+      this.quitButton.style("left", "65%")
       this.quitButton.mousePressed(this.quitGame);
       pop();
     }
     push();
+    background(172, 184, 229, 245);
     textAlign(CENTER);
     fill("rgb(242,37,174)");
     stroke("rgb(5,42,147)");
     strokeWeight(12);
     noCursor();
     ellipse(mouseX, mouseY, 30, 30);
-    background(172, 184, 229, 10);
-    image(gameOver, 15, 125);
+    image(gameOver, 175, 35);
+    gameOver.resize(240, 180);
     pop();
   }
 
