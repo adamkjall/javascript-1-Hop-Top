eb7073f23536a4c42748104aa9df9c41ca3c8cc1
Claudia Aranda <claudia_15_@hotmail.com>
2020-01-07 02:14:46 +0100

    cant make this work but i need to save to merge with current master

--- sketch/gameController.ts
+++ sketch/gameController.ts
@@ -9,8 +9,7 @@ class GameController {
   private isStartingNextLevel: boolean;
   private countDown: number;
   private isStartGame: boolean;
-  private playButton: p5.Element | undefined;
-  private quitButton: p5.Element | undefined;
+
 
   constructor() {
     this.score = 0;
@@ -149,68 +148,6 @@ class GameController {
     fill(32);
     text("Next level in " + this.countDown, width / 2, height / 4);
     pop();
-  }
-
-  private displayGameOver() {
-    if (!this.playButton && !this.quitButton) {
-      push();
-      //if clicked go to level1
-
-      this.playButton = createButton("PLAY AGAIN?");
-      this.playButton.position(windowWidth / 2, height * 0.82);
-      this.playButton.center("horizontal");
-      this.playButton.style("background-color", "rgb(252, 208, 107)");
-      this.playButton.style('font-family', 'Amatic SC');
-      //textFont(font);
-      this.playButton.style("font-size", "2rem");
-      this.playButton.style("color", "rgb(38,48,86)");
-      this.playButton.style("border-radius", "2rem");
-      this.playButton.style("padding", "1rem");
-      this.playButton.style("border", "none");
-      this.playButton.style("outline", "none");
-
-      this.playButton.mousePressed(this.restartGame);
-
-      //if clicked go to startScreen?
-      this.quitButton = createButton("QUIT");
-      this.quitButton.position(windowWidth / 2, height * 0.94);
-      this.quitButton.center("horizontal");
-      this.quitButton.style("background-color", "rgb(38,48,86)");
-      this.quitButton.style('font-family', 'Amatic SC');
-      //textFont(font);
-      this.quitButton.style("font-size", "1.7rem");
-      this.quitButton.style("color", "rgb(252, 208, 107)");
-      this.quitButton.style("border-radius", "1rem");
-      this.quitButton.style("border", "none");
-      this.quitButton.style("outline", "none");
-      this.quitButton.style("display", "grid");
-      this.quitButton.style("justify-items", "center");
-      this.quitButton.mousePressed(this.quitGame);
-      pop();
-    }
-    push();
-    //textFont(font);
-    textFont("Amatic SC");
-    textAlign(CENTER);
-    fill("rgb(242,37,174)");
-    stroke("rgb(5,42,147)");
-    strokeWeight(12);
-    noCursor();
-    ellipse(mouseX, mouseY, 30, 30);
-    background(172, 184, 229, 10);
-    image(gameOver, 15, 125);
-    pop();
-  }
-
-
-
-  private restartGame(): void {
-    removeElements();
-    gameController = new GameController();
-  }
-
-  private quitGame(): void {
-    location.reload();
   }
 
   private updateScore(itemScore: number): void {
